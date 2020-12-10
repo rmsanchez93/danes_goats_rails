6a87c48029ef31600546eb917727921e347f75db
Your Name <you@example.com>
2020-12-09 21:24:20 -0600

    basic notes added

--- goat_app/app/controllers/goats_controller.rb
+++ goat_app/app/controllers/goats_controller.rb
@@ -16,7 +16,7 @@ class GoatsController < ApplicationController
         @goat = Goat.create(goat_params)
         redirect_to goats_path
     end
-
+# because I added the selector in the new goat form, it collects a person_id to include in the params.
     def goat_params
         params.require(:goat).permit(:name, :age, :person_id, :img)
     end
